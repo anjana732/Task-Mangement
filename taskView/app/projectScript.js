8eb00e073331cc75cefa5fa9fee16397a5e468f7
anjana732 <anjanasingh1257@gmail.com>
2024-10-30 12:42:50 +0530

    Add Functionality to Project page

--- taskView/app/projectScript.js
+++ taskView/app/projectScript.js
@@ -31,4 +31,64 @@ window.onclick = function(event) {
 };
 
 
-let edit = document.getElementById('edit')
+let edit = document.getElementById('edit');
+let form = document.getElementById('desc-form');
+edit.addEventListener('click',function(){
+    document.getElementById('desc-form').style.display = 'block';
+    edit.style.display = 'none';
+})
+
+let cancelBtn = document.getElementById('cancel-btn');
+cancelBtn.addEventListener('click',function(){
+    
+    document.getElementById('desc-form').style.display = 'none';
+})
+
+let saveBtn = document.getElementById('save-btn');
+const descriptionEditior = document.getElementById('description-editor');
+const descriptionText = null;
+saveBtn.addEventListener('click',function(){
+    descriptionText = descriptionEditior.value;
+    console.log(descriptionText);
+    document.getElementById('desc-form').style.display = 'none';
+})
+
+let extend = document.getElementById('extend');
+let savedDesc =  document.getElementById('saved-desc');
+extend.addEventListener('click',function(){
+    if(descriptionText==null){
+        savedDesc.innerHTML = `No Description Available`;
+    }else{
+        savedDesc.innerHTML = descriptionText;
+    }
+    if(savedDesc.style.display == 'none'){
+        document.getElementById('saved-desc').style.display = 'block';
+    }else{
+        savedDesc.style.display = 'none';
+    }
+})
+
+function showTab(tabId) {
+    
+    const tabPanes = document.querySelectorAll('.tab-pane');
+    tabPanes.forEach(pane => pane.classList.remove('active'));
+
+   
+    const tabButtons = document.querySelectorAll('.tab-button');
+    tabButtons.forEach(button => button.classList.remove('active'));
+
+    document.getElementById(tabId).classList.add('active');
+    event.target.classList.add('active');
+}
+
+let expandTask = document.getElementById('expand-task');
+let expandTaskList = document.getElementById('task-expand-list');
+
+expandTask.addEventListener('click',function(){
+    if (expandTaskList.style.display === 'block') {
+        expandTaskList.style.display = 'none';
+    } else {
+        expandTaskList.style.display = 'block';
+    }
+})
+
